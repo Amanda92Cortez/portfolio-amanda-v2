e2d10d5fb50ed2d8564d45c7b15d47df71fc724f
Amanda92Cortez <amanda_cortez_2012@hotmail.com>
2025-02-26 15:43:36 -0300

    Projetos

--- js/script.js
+++ js/script.js
@@ -13,3 +13,20 @@ function filtrarProjetos(categoria) {
         }
     });
 }
+
+// Modal
+document.querySelectorAll(".openModal").forEach(item => {
+    item.addEventListener("click", function() {
+        let projeto = this.closest(".projeto");
+        document.getElementById("modal-title").textContent = projeto.dataset.title;
+        document.getElementById("modal-image").src = projeto.dataset.image;
+        document.getElementById("modal-description").textContent = projeto.dataset.description;
+        document.getElementById("modal-languages").textContent = "Linguagens: " + projeto.dataset.languages;
+        document.getElementById("modal-link").href = projeto.dataset.link;
+        document.getElementById("modal").style.display = "flex";
+    });
+});
+
+document.querySelector(".close").addEventListener("click", function() {
+    document.getElementById("modal").style.display = "none";
+});
