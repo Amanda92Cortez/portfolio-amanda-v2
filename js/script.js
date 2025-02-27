7128f698c19f24c8a24682e0dbdf33d64acd4633
Amanda92Cortez <amanda_cortez_2012@hotmail.com>
2025-02-27 16:26:11 -0300

    Projetos

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
 function filtrarProjetos(categoria) {
-    let projetos = document.querySelectorAll(".projeto");
+    let projetos = document.querySelectorAll('.projeto');
 
     projetos.forEach(projeto => {
-        if (categoria === "todos") {
-            projeto.style.display = "block";
+        if (categoria === 'todos' || projeto.dataset.categoria === categoria) {
+            projeto.style.display = 'block'; // Mostra o projeto
         } else {
-            if (projeto.getAttribute("data-categoria") === categoria) {
-                projeto.style.display = "block";
-            } else {
-                projeto.style.display = "none";
-            }
+            projeto.style.display = 'none'; // Esconde o projeto
         }
     });
 }
